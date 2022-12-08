35de28ea6d46c215b3e42474fe58353a6e4e7691
Sophia Edeki <sophiesse143@gmail.com>
2022-12-08 03:08:30 +0100

    used props to create reuseable data

--- airbnb/src/components/card.js
+++ airbnb/src/components/card.js
@@ -1,24 +1,28 @@
-import card from '../assets/card.png';
 import star from '../assets/star.png';
+import '../card.png'
+import '../card2.png'
+import '../card3.png'
 import '../App.css';
 
-function Card() {
+function Card(props) {
   return (
     <section >
+      <div className='Card--container'>
+
         <div className='Card--details'>
 
-            <img src={card} className="Card--image" alt="Card" />
+            <img src={props.img} className="Card--image" alt="Card" />
             <div className='Ratings'>
             <img src={star} className="Star--image" alt="Ratings" />
-            <span>5.0</span>
-            <span className='Card--rating'>(6) . </span>
+            <span>{props.rating1}</span>
+            <span className='Card--rating'>{props.rating2} </span>
             <span className='Card--rating'>USA</span>
             </div>
-            <p className='Card--desc'>Life lessons with Katie Zaferes
-    </p>
-            <p className='Card--cost'><span>From $136 </span>/ person
-    </p>
+            <p className='Card--desc'>{props.cardDesc}</p>
+            <p className='Card--cost'><span>{props.cardCost} </span>/ person
+            </p>
         </div>
+      </div>
     </section>
   );
 }
--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Card from './components/card';
+import './card.png'
+import './card2.png'
+import './card3.png'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +14,28 @@ root.render(
   <React.StrictMode>
     <Navbar />
     <Hero />
-    <Card />
+    <Card
+      img = "./card.png"
+      rating1 = "5.0"
+      rating2 = "(6)"
+      rating3 = "USA"
+      cardDesc = "Life lessons with Katie Zaferes"
+      cardCost = "From $136"
+    />
+    <Card
+       img= "./card2.png"
+       rating1 = "5.0"
+       rating2 = "(30)"
+       cardDesc = "Learn Wedding Photography"
+       cardCost = "From $125"
+    />
+    <Card 
+       img = "./card3.png"
+       rating1 = "4.8"
+       rating2 = "(2)"
+       cardDesc = "Group Mountain Bikin"
+       cardCost = "From $50"
+    />
   </React.StrictMode>
 );
 
